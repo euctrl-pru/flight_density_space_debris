3703a763563e5a85f2b4dc6391f98cf3396a2064
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-02-07 16:02:41 +0100

    use bbox to reduce file size

--- R/export_data.R
+++ R/export_data.R
@@ -4,14 +4,55 @@ library(conflicted)
 library(tidyverse)
 library(eurocontrol)
 library(arrow)
+library(sf)
+library(giscoR)
+library(sf)
+library(smoothr)
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(h3jsr)
+library(magrittr)
 
 withr::local_envvar(c(TZ = "UTC", ORA_SDTZ = "UTC", NLS_LANG = ".AL32UTF8"))
 conn <- withr::local_db_connection(db_connection("PRU_DEV"))
 
-bb <- c(xmin = -25.488281,
-        xmax = 45.407181,
-        ymin = 26.638253,
-        ymax = 71.864780)
+
+
+# bbox Europe/EUROCONTROL area
+# -25.488281,26.638253,45.407181,71.864780
+bb_nm <- c(
+  xmin = -25.488281,
+  ymin =  26.638253,
+  xmax =  45.407181,
+  ymax =  71.864780) |>
+  st_bbox(crs = 4326) |>
+  st_as_sfc(crs = 4326) |>
+  # add points to the bbox polygon
+  densify(n = 300L) |>
+  st_as_sf(res = 2)
+
+
+
+res <- 2
+cells <- bb_nm |>
+  st_transform(crs = 4326) |>
+  polygon_to_cells(res = res, simple = FALSE)
+
+eur_hex <- cells |>
+  pull(h3_addresses) |>
+  unlist() |>
+  cell_to_polygon(simple = FALSE)
+
+# take the bbox of the union of hexes at resolution 2 as the bbox for the query
+eur_hex_union <- eur_hex |>
+  st_union() |>
+  st_exterior_ring() |>
+  st_as_sf() |>
+  mutate(resolution = res)
+
+bb <- eur_hex |>
+  st_bbox()
 
 
 # Aug 2024
@@ -21,7 +62,7 @@ til <- wef + ddays(1)
 trjs <- point_profiles_tidy(conn = conn,
                     wef = wef,
                     til = til,
-                    # bbox = bb,
+                    bbox = bb,
                     profile = "CTFM")
 
 trjs |>
@@ -36,7 +77,7 @@ til <- wef + ddays(1)
 trjs <- point_profiles_tidy(conn = conn,
                             wef = wef,
                             til = til,
-                            # bbox = bb,
+                            bbox = bb,
                             profile = "CTFM")
 
 trjs |>
