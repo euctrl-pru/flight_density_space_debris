5b45b0c58b9bebe6347dad998fec10502db5f843
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-02-09 17:11:27 +0100

    record alternative computation for occupancy

--- R/prepare_density.R
+++ R/prepare_density.R
@@ -7,14 +7,8 @@ library(h3jsr)
 
 conflict_prefer("filter", "dplyr", quiet = TRUE)
 
-source(here("R", "cells.R"))
-
-res <- 3
-eur_hex_union <- bbox_nm() |>
-  cells_boundary_at_res(resolution = res)
-
 # circa 15 secs
-dd <- c(
+c(
   "data/trajectories_2024-08-01_resampled_30s_bbox_res_2.parquet",
   "data/trajectories_2024-12-05_resampled_30s_bbox_res_2.parquet",
   "data/trajectories_2024-08-01_resampled_30s_bbox_res_3.parquet",
@@ -26,3 +20,30 @@ dd <- c(
             .by = c(year, month, day, hour, h3_resolution, cell)) |>
   arrange(desc(occupancy)) |>
   write_parquet(here("data", "flight_density.parquet"), compression = "gzip")
+
+
+
+# # Alternative (but far slower) computation of density
+# c(
+#   "data/trajectories_2024-08-01_resampled_30s_bbox_res_2.parquet",
+#   "data/trajectories_2024-12-05_resampled_30s_bbox_res_2.parquet",
+#   "data/trajectories_2024-08-01_resampled_30s_bbox_res_3.parquet",
+#   "data/trajectories_2024-12-05_resampled_30s_bbox_res_3.parquet",
+#   NULL) |>
+#   purrr::map(.f = read_parquet) |>
+#   dplyr::bind_rows() |>
+#   dplyr::group_by(year, month, day, hour, h3_resolution, cell, flight_id) |>
+#   dplyr::arrange(timestamp) |>
+#   dplyr::mutate(
+#     sequence_id_next = lead(sequence_id),
+#     timestamp_next   = lead(timestamp),
+#     is_consecutive   = if_else(is.na(sequence_id_next),
+#                                FALSE,
+#                                (sequence_id_next - sequence_id) < 1),
+#     duration_segment = if_else(is_consecutive,
+#                                (timestamp_next - timestamp) |> as.duration(),
+#                                seconds(30) |> as.duration())
+#     ) |>
+#   ungroup() |>
+#   group_by(cell, hour) |>
+#   summarise(density = sum(duration_segment) / 3600)
