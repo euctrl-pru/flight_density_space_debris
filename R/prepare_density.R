4b1525a9ddcc2bd62dcdfd36d1fabeee8cf34177
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-02-09 13:57:48 +0100

    refine density

--- R/prepare_density.R
+++ R/prepare_density.R
@@ -9,18 +9,21 @@ conflict_prefer("filter", "dplyr", quiet = TRUE)
 
 source(here("R", "cells.R"))
 
-res <- 2
-eur_hex_union <- bbox_nm() |> cells_boundary_at_res(resolution = res)
+res <- 3
+eur_hex_union <- bbox_nm() |>
+  cells_boundary_at_res(resolution = res)
 
-# circa 30 min
+# circa 15 secs
 # BOGUS? Just assuming that 1 position in cell is equal to (max) 30s flown
 dd <- c(
-  "data/trajectories_2024-08-01_resampled_30s_bbox.parquet",
-  "data/trajectories_2024-12-05_resampled_30s_bbox.parquet",
+  "data/trajectories_2024-08-01_resampled_30s_bbox_res_2.parquet",
+  "data/trajectories_2024-12-05_resampled_30s_bbox_res_2.parquet",
+  "data/trajectories_2024-08-01_resampled_30s_bbox_res_3.parquet",
+  "data/trajectories_2024-12-05_resampled_30s_bbox_res_3.parquet",
   NULL) |>
   map(.f = read_parquet) |>
   bind_rows() |>
-  summarise(.by = c(cell, year, month, day, hour),
+  summarise(.by = c(cell, year, month, day, hour, h3_resolution),
             occupancy = n() * 30 / 3600) |>
   arrange(desc(occupancy)) |>
   write_parquet(here("data", "flight_density.parquet"), compression = "gzip")
