ab47954dacb5db31903ee44e9565aa05a227e388
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-04-06 16:22:57 +0200

    keplerian orbits helper

---
+++ R/orbitals.R
@@ -0,0 +1,97 @@
+#' convert from orbital elements to cartesian coordinates
+#'
+#' @param nu the true anomaly, i.e. the angular displacement measured from
+#'           periapsis to the position vector along the direction of motion
+#' @param a  the semimajor axis of the orbit
+#' @param e  the eccentricity of the orbit
+#' @param i the inclination of the orbit
+#' @param Omega the Right ascension of the ascending node
+#' @param w the argument of perigee
+#' @param M the mass of the first body (more massive one), i.e. Earth
+#' @param m the mass of the orbiting body (less massive), i.e. satellite.
+#'          In the case of m << M, m can be passed as 0
+#'
+#' @returns a vector with I, J, K, Vi, Vj, Vk of the values of position and
+#'          velocity in a geocentric-equatorial reference system
+#'
+#' @examples
+#' \dontrun{
+#' rE  <- 6378e3 # Earth equatorial radius
+#' alt <- 550e3 # altitude of satellite, i.e. 500 km for LEO
+#' ecc <- 0 # circular orbit, i.e. when uncontrolled reentry of debris
+#' m   <- 0 # mass of debris/satellite << mass Earth, so it can be 0
+#' o   <- 0
+#' w   <- 0
+#' i <- 10
+#' kepler_to_cartesian(
+#'   nu = 0.5,
+#'   a = rE + alt,
+#'   e = ecc,
+#'   i = i, Omega = o,
+#'   w = w,
+#'   m = m)
+#' }
+kepler_to_cartesian <- function(
+  nu,
+  a,
+  e,
+  i,
+  Omega,
+  w,
+  M = 5.9722e24, # mass of the major body, i.e. Earth
+  m = 0 # mass of the other body, i.e. satellite or debris
+) {
+  G <- 6.67430e-11 # gravitational constant
+  mu <- G * (M + m) #standard gravitational parameter
+
+  p <- a * (1 - e^2) # semiparameter (semilatus rectum)
+  r_0 = p / (1 + e * cos(nu))
+
+  #--------------- Coordinates in the perifocal reference system Oxyz -----------------
+  # position vector coordinates
+  x <- r_0 * cos(nu)
+  y <- r_0 * sin(nu)
+  # velocity vector coordinates
+  Vx_ <- -sqrt(mu / p) * sin(nu)
+  Vy_ <- sqrt(mu / p) * (e + cos(nu))
+
+  #-------------- the geocentric-equatorial reference system OIJK ---------------------
+  # position vector components I, J, and K
+  # fmt: skip
+  {
+    cosO <- cos(Omega)
+    sinO <- sin(Omega)
+    cosw <- cos(w)
+    sinw <- sin(w)
+    cosi <- cos(i)
+    sini <- sin(i)
+    I = (cosO * cosw - sinO * sinw * cosi) * x + (-cosO * sinw - sinO * cosw * cosi) * y
+    J = (sinO * cosw + cosO * sinw * cosi) * x + (-sinO * sinw + cosO * cosw * cosi) * y
+    K = (sinw * sini) * x                                    + (cosw * sini) * y
+    # velocity vector components I', J', and K'
+    Vi = (cosO * cosw - sinO * sinw * cosi) * Vx_ + (-cosO * sinw - sinO * cosw * cosi) * Vy_
+    Vj = (sinO * cosw + cosO * sinw * cosi) * Vx_ + (-sinO * sinw + cosO * cosw * cosi) * Vy_
+    Vk = (sinw * sini) * Vx_                                    + (cosw * sini) * Vy_
+  }
+  return
+  c(i = I, j = J, k = K, vi = Vi, vj = Vj, vk = Vk)
+}
+
+# test
+# rE <- 6378e3 # Earth equatorial radius
+# alt <- 550e3 # altitude of satellite, i.e. 500 km for LEO
+# ecc <- 0 # circlar orbit when uncontrolled reentry of debris
+# mSat <- 0 # mass of debris/satellite << mass Earth, so it can be 0
+# Omega <- 0
+# w <- 0
+# i <- 10
+#
+# kepler_to_cartesian(
+#   nu = 0.5,
+#   a = rE + alt,
+#   e = 0,
+#   i = i,
+#   Omega = Omega,
+#   w = w,
+#   m = mSat
+# )
