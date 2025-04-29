a3bf7ad52dc729daf95beee4452c460a8479b9ce
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-04-29 15:54:49 +0200

    plot U-shaped impact probability density  for debris of inclination `i`

---
+++ R/plot_weighting_function_inclination_i.R
@@ -0,0 +1,30 @@
+# plot U-shaped impact probability density for debris of inclination `i`
+
+library(tidyverse)
+library(aviodebris)
+
+# Earth equatorial radius
+rE <- 6378e3
+# altitude of satellite from (spherical) Earth surface, i.e. 500 km for LEO
+alt <- 550e3
+
+# inclination of Kosmos 482 Descent Craft (1972-023E, cat. nr. 6073)
+# see https://sattrackcam.blogspot.com/2025/04/kosmos-842-descent-craft-reentry.html
+inclination_deg = 51.7
+
+www <- latitude_weights(
+  delta_lat = 0.5,
+  altitude = rE + alt,
+  inclination_deg = inclination_deg
+)
+www |>
+  ggplot() +
+  geom_line(aes(x = lat, y = val), linewidth = 1.3) +
+  scale_x_continuous("latitude", breaks = c(-90, -60, -30, 0, 30, 60, 90)) +
+  theme_minimal(base_size = 24) +
+  theme(
+    axis.title.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+  )
+
+ggsave("media/figures/weighting_inclination_i.png")
